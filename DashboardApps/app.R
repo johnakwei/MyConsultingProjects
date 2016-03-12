6a275e5dbf711a42114cfc6cf5daaab158d79cbe
John Akwei <johnakwei1@gmail.com>
2016-03-12 08:47:05 -0500

    uploading

--- DashboardApps/app.R
+++ DashboardApps/app.R
@@ -1,201 +1,186 @@
-# setwd("C:/Users/johnakwei/Dropbox/Programming/Dashboard/Venture_Capital_Dashboard")
+setwd("C:/Users/johnakwei/Dropbox/Programming/Dashboard/ContextBase_2nd_Dashboard")
 
-# shinyapps::deployApp("C:/Users/johnakwei/Dropbox/Programming/Dashboard/Venture_Capital_Dashboard")
-
+# install.packages("shiny")
+# install.packages("shinydashboard")
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
-
-firmData <- read.csv("Clean_Series_A_Research_Calculations.csv", header=T)
-firmData$Date <- strptime(firmData$Date, format="%m/%d/%Y")
-firmData$InvestmentAmount <- as.numeric(gsub("[/|$|A-J|a-j|L-Z|l-z|()]",
-                                             "", firmData$Investment))
-firmData$InvestmentType <- gsub("[/|$|0-9|.|k|B|M]", "", firmData$Investment)
-firms <- unique(firmData$VentureFirm)
-Investments <- aggregate(firmData$InvestmentAmount~firmData$InvestmentType,
-                         FUN=sum)
-names(Investments) <- c("Type", "Amount")
-
-StartUpInvestAvg <- tapply(firmData$InvestmentAmount,
-                           firmData$StartUp, FUN=sum, na.rm=T)
-
-VCInvestAvg <- tapply(firmData$InvestmentAmount,
-                      firmData$VentureFirm, FUN=sum, na.rm=T)
-
-# TimeConfig <- firmData[firmData$Date %in%
-#                        c("2014-01-01", "2014-02-01", "2014-03-01", "2014-04-01",
-#                          "2014-05-01", "2014-06-01", "2014-07-01", "2014-08-01",
-#                          "2014-09-01", "2014-10-01", "2014-11-01", "2014-12-01",
-#                          "2015-01-01", "2015-02-01", "2015-03-01", "2015-04-01",
-#                          "2015-05-01", "2015-06-01", "2015-07-01", "2015-08-01",
-#                          "2015-09-01", "2015-10-01", "2015-11-01", "2015-12-01"),]
-
-OutputProcess <- function(VentureFirm, StartUp, InvestType) {
-  if (VentureFirm=="All") {VentureFirm <- firmData$VentureFirm}
-  if (StartUp=="All") {StartUp <- firmData$StartUp}
-  if (InvestType=="All") {InvestType <- firmData$InvestmentType}
-  out <- firmData[firmData$VentureFirm==VentureFirm &
-                    firmData$StartUp==StartUp &
-                    firmData$InvestmentType==InvestType,]
-  out
-}
+absorbance1 <- read.csv("Absorbances1.csv", header=T)
 
 ui <- dashboardPage(  
-  dashboardHeader(title="VC Dashboard"),
+  dashboardHeader(title="Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard 1", tabName="dashboard1", icon=icon("dashboard")),
-      menuItem("Venture Capital Search", tabName="venturesearch",
-               icon=icon("dashboard")),
-      menuItem("Documentation", tabName="documentation", icon=icon("th"))
+      menuItem("Dashboard 1", tabName="dashboard", icon=icon("dashboard")),
+      menuItem("Dashboard 2", tabName="dashboard2", icon=icon("th")),
+      dropdownMenu(type = "messages",
+                   messageItem(
+                     from = "Marketing Dept",
+                     message = "Sales are steady this month."
+                   ),
+                   messageItem(
+                     from = "New Client",
+                     message = "Request new project",
+                     icon = icon("question"),
+                     time = "13:45"
+                   ),
+                   messageItem(
+                     from = "Project Management",
+                     message = "Completion of new Milestone",
+                     icon = icon("life-ring"),
+                     time = "2014-12-01"
+                   )
+      )
     )
   ),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName="dashboard1",
+      tabItem(tabName="dashboard",
               fluidRow(
-                box(plotOutput("plot1", height=400)),
-                box(print(
-                  paste("The average of All VC investments per type is",
-                        round(mean(Investments$Amount)), " Million USD.")),
-                  height=100)
+                box(plotOutput("plot1", height=300)),
+                box(plotOutput("plot2", height=300))
                 ),
               fluidRow(
-                box(plotOutput("plot2", height=400)),
-                box(print(
-                  paste("The average total Venture Capitalization of a StartUp is",
-                        round(mean(StartUpInvestAvg, na.rm=T)),
-                        " Million USD.")), height=100)
-                ),
-              fluidRow(
-                box(plotOutput("plot3", height=400)),
-                box(print(paste("The average total investments of Venture Capital Firms is",
-                                round(mean(VCInvestAvg, na.rm=T)),
-                                " Million USD.")), height=100)
+                box(plotOutput("plot3", height=250)),
+                box(
+                  title="Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
                 )
-#               fluidRow(
-#                 box(plotOutput("plot4", height=400))
-#                 )
-    ),
-    tabItem(tabName="venturesearch",
-            fluidRow(
-              box(title="Venture Capital Information Search",
-                  actionButton("VCSearch", "Search"),
-                  selectInput("VCSelector", "Select Venture Capital Firm:",
-                              as.character(firmData$VentureFirm),
-                              selected="Norwest", width="100%"),
-                  uiOutput("StartUpSelector"),
-                  uiOutput("InvestTypeSelector")),
-                  box(textOutput('text1'))
+              ),
+              fluidRow(
+                box(plotOutput("plot4", height=300)),
+                box(
+                  title="Controls",
+                  radioButtons("radioButtons", "Number of observations:",
+                                     c("1"=1, "50"=50, "100"=100),
+                                     selected="50", width="100%")
+                )
               )
       ),
-    tabItem(tabName="documentation",
-            fluidRow(
-              box(h4(print(paste("Introduction")), height=100))),
-            fluidRow(
-              box(h4(print(paste("This dashboard application
-                                 presents an overview of Venture Capital
-                                 Firm data, and allows for specific 
-                                 venture capitalization case
-                                 searching.")), height=100))
+      # Second tab content
+      tabItem(tabName="dashboard2",
+              h2("Dashboard 2"),
+              fluidRow(
+                box(plotOutput("plot5", height=400)),
+                box(
+                  title="Controls",
+                  radioButtons("radioButtons2", "Number of observations:",
+                               c("100"=100, "500"=500, "1000"=1000),
+                               selected="500", width="100%")
+                )
               ),
-            fluidRow(
-              box(h4(print(paste("Features")), height=100))),
-            fluidRow(
-              box(h4(print(paste("Dashboard 1: Generates graphs of
-                                  Sums of Investment Types,
-                                  StartUp Investments,
-                                  Sums of Investment Firms.")), height=100))),
-            fluidRow(
-              box(h4(print(paste("Venture Capital Search:
-                                 Allows searching of specific
-                                 Venture Capital cases by
-                                 Investment Firm, StartUp, and
-                                 Investment Type.")), height=100)))
-          )
-      )
+              fluidRow(
+                box(plotOutput("plot6", height=400)),
+                box(
+                  title="Absorbance Graph Controls",
+                  radioButtons("radioButtons3", "Select Sample 1-7:",
+                               c("Sample 1"="absorbance1$Sample_1",
+                                 "Sample 2"="absorbance1$Sample_2",
+                                 "Sample 3"="absorbance1$Sample_3",
+                                 "Sample 4"="absorbance1$Sample_4",
+                                 "Sample 5"="absorbance1$Sample_5",
+                                 "Sample 6"="absorbance1$Sample_6",
+                                 "Sample 7"="absorbance1$Sample_7"),
+                               selected="absorbance1$Sample_1", width="100%")
+                )
+              )
+              )
+    )
   )
 )
 
 server <- function(input, output) {  
   
   output$plot1 <- renderPlot({
-    ggplot(data=Investments, aes(Type, Amount)) +
-      geom_bar(aes(), stat="identity", fill=rainbow(27)) +
-      ggtitle("All VC Investments per Type") +
-      xlab("") + ylab("Investment Sum (Millions)") +
-      theme(plot.title=element_text(color="blue")) +
-      theme(axis.title=element_text(color="darkblue")) +
-      theme(axis.text.x=element_text(angle=45, hjust=1))
+    dataset <- matrix(nrow=20, ncol=4)
+    
+    colnames(dataset) <- c("Year", "Sales", "Profits", "Costs")
+    
+    dataset[3,] <- c(2002, 490, 85.46, 404.5)
+    dataset[4,] <- c(2003, 425, 76.84, 348.2)
+    dataset[5,] <- c(2004, 480, 89.86, 390.1)
+    dataset[6,] <- c(2005, 480, 82.18, 397.8)
+    dataset[7,] <- c(2006, 405, 71.93, 333.1)
+    dataset[8,] <- c(2007, 430, 66.74, 363.3)
+    dataset[9,] <- c(2008, 485, 83.03, 402.0)
+    dataset[10,] <- c(2009, 475, 88.16, 386.8)
+    dataset[11,] <- c(2010, 425,  55.76, 369.2)
+    dataset[12,] <- c(2011, 430,  69.49, 360.5)
+    
+    dataset <- data.frame(dataset)
+    
+    plot_sales <- ggplot(dataset,aes(x=factor(Year),y=Sales,fill=Sales)) +
+      xlab('Year') + ylab('Sales ($)') +
+      geom_bar(stat='identity',colour='black') +
+      scale_fill_gradient(low='red',high='green') +
+      ggtitle('Yearly sales') +
+      theme(axis.text.x = element_text(angle = -35))
+    
+    plot_sales
   })
   
   output$plot2 <- renderPlot({
-    StartUpInvestAvg <- tapply(firmData$InvestmentAmount,
-                               firmData$StartUp, FUN=sum, na.rm=T)
-    plot(StartUpInvestAvg, main="Average Investment per StartUp (2434 StartUps)",
-         col.main="blue", xlab="StartUp", ylab="Investment (Millions)",
-         col.lab="darkblue", col=firmData$StartUp)
+    dataset <- matrix(nrow=20, ncol=4)
+    
+    colnames(dataset) <- c("Year", "Sales", "Profits", "Costs")
+    
+    dataset[3,] <- c(2002, 490, 85.46, 404.5)
+    dataset[4,] <- c(2003, 425, 76.84, 348.2)
+    dataset[5,] <- c(2004, 480, 89.86, 390.1)
+    dataset[6,] <- c(2005, 480, 82.18, 397.8)
+    dataset[7,] <- c(2006, 405, 71.93, 333.1)
+    dataset[8,] <- c(2007, 430, 66.74, 363.3)
+    dataset[9,] <- c(2008, 485, 83.03, 402.0)
+    dataset[10,] <- c(2009, 475, 88.16, 386.8)
+    dataset[11,] <- c(2010, 425,  55.76, 369.2)
+    dataset[12,] <- c(2011, 430,  69.49, 360.5)
+    
+    dataset <- data.frame(dataset)
+    
+    plot_profits <- ggplot(dataset,aes(x=factor(Year),y=Profits,fill=Profits)) +
+      xlab('Year') + ylab('Profits ($)') +
+      geom_bar(stat='identity',colour='black') +
+      scale_fill_gradient(low='red',high='green') +
+      ggtitle('Yearly profits') +
+      theme(axis.text.x = element_text(angle = -35))
+    
+    plot_profits
   })
+  
+  set.seed(122)
+  histdata <- rnorm(500)
   
   output$plot3 <- renderPlot({
-    VCInvestAvg <- tapply(firmData$InvestmentAmount,
-                          firmData$VentureFirm, FUN=sum, na.rm=T)
-    plot(VCInvestAvg,
-         main="Average Investment per Venture Capital Firm (115 VCs)",
-         col.main="blue", xlab="Venture Capital Firm",
-         ylab="Investments (Millions)", col.lab="darkblue",
-         col=firmData$VentureFirm)
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
   })
   
-#   output$plot4 <- renderPlot({
-#     TimeConfig <- firmData[firmData$Date %in%
-#                              c("2014-01-01", "2014-02-01", "2014-03-01", "2014-04-01",
-#                                "2014-05-01", "2014-06-01", "2014-07-01", "2014-08-01",
-#                                "2014-09-01", "2014-10-01", "2014-11-01", "2014-12-01",
-#                                "2015-01-01", "2015-02-01", "2015-03-01", "2015-04-01",
-#                                "2015-05-01", "2015-06-01", "2015-07-01", "2015-08-01",
-#                                "2015-09-01", "2015-10-01", "2015-11-01", "2015-12-01"),]
-#     
-#     ggplot(TimeConfig, aes(TimeConfig$Date, TimeConfig$InvestmentAmount)) +
-#       stat_summary(geom="line", fun.y="sum", color="green") +
-#       geom_point(shape=1, col="green") +
-#       xlab("") + ylab("VC Investments (Millions)") +
-#       ggtitle("All Venture Capital Investments 2014-2015") +
-#       theme(plot.title=element_text(color="blue")) +
-#       theme(axis.title=element_text(color="darkblue"))
-#     })
-  
-  output$StartUpSelector <- renderUI({
-    StartUpList <- subset(firmData$StartUp,
-                          firmData$VentureFirm==input$VCSelector)
-    selectInput("StartUps", "Select StartUp:",
-                as.character(StartUpList))})
-  
-  output$InvestTypeSelector <- renderUI({
-    InvestList <- subset(firmData$InvestmentType,
-                         firmData$VentureFirm==input$VCSelector)
-    InvestList <- subset(firmData$InvestmentType,
-                         firmData$StartUp==input$StartUps)
-    selectInput("InvestType", "Select Investment Type:",
-                as.character(InvestList))})
-
-  output$text1 <- renderPrint({
-    input$VCSearch
-    isolate({
-      OutputProcess <- function(VentureFirm, StartUp, InvestType) {
-        if (VentureFirm=="All") {VentureFirm <- firmData$VentureFirm}
-        if (StartUp=="All") {StartUp <- firmData$StartUp}
-        if (InvestType=="All") {InvestType <- firmData$InvestmentType}
-        out <- firmData[firmData$VentureFirm==VentureFirm &
-                          firmData$StartUp==StartUp &
-                          firmData$InvestmentType==InvestType,]
-        out
-      }
-    OutputProcess(input$VCSelector, input$StartUps,
-                            input$InvestType)})
+  output$plot4 <- renderPlot({
+    data2 <- histdata[seq_len(input$radioButtons)]
+    hist(data2)
   })
   
+  output$plot5 <- renderPlot({
+    set.seed(100)
+    d <- diamonds[sample(nrow(diamonds), input$radioButtons2), ]
+    
+    ggplot(data=d, aes(x=carat, y=price)) +
+      geom_point(aes(text=paste("Clarity:", clarity)), size=4) +
+      geom_smooth(aes(colour=cut, fill=cut)) + facet_wrap(~cut)
+  })
+  
+  output$plot6 <- renderPlot({
+    absorbance1$Powder_mg_vs_Absorbance_E_coli <-
+      as.numeric(absorbance1$Powder_mg_vs_Absorbance_E_coli)    
+    ggplot(absorbance1,
+           aes_string(x=as.factor(absorbance1$Powder_mg_vs_Absorbance_E_coli),
+                            y=input$radioButtons3)) +
+      geom_boxplot(aes(fill=as.factor(absorbance1$Powder_mg_vs_Absorbance_E_coli))) +
+      ggtitle("E.Coli Absorbance per MG - Medians") +
+      xlab("Quantities in Milligrams") + ylab("E.Coli Absorbance") +
+      labs(fill="Milligrams")
+  })
 }
 
 shinyApp(ui, server)
