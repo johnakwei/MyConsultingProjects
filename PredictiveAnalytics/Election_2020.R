a751d56150813e3a7d256f5caef7ea3c128eeb2a
John Akwei <johnakwei1@gmail.com>
2020-09-26 10:01:46 -0400

    Update Election_2020.R

--- PredictiveAnalytics/Election_2020.R
+++ PredictiveAnalytics/Election_2020.R
@@ -15,37 +15,35 @@ head(election2)
 ggplot(data = election2) +
   geom_line(aes(x = Date, y = ElectoralBiden, group=1,
                 color = "ElectoralBiden"), size = 2) +
-  geom_text(aes(x = max(Date), y = max(ElectoralBiden),
-                label = round(max(ElectoralBiden), 0))) +
+  geom_text(aes(x = max(Date), y = ElectoralBiden[Date == max(Date)],
+                label = round(ElectoralBiden[Date == max(Date)], 0))) +
   geom_line(aes(x = Date, y = ElectoralTrump, group=1,
                 color = "ElectoralTrump"), size = 2) +
-  geom_text(aes(x = max(Date), y = max(ElectoralTrump),
-                label = round(max(ElectoralTrump), 0))) +
+  geom_text(aes(x = max(Date), y = ElectoralTrump[Date == max(Date)],
+                label = round(ElectoralTrump[Date == max(Date)], 0))) +
   labs(x="Date", y="Electoral Votes",
        title="Election 2020 - Electoral Vote",
        caption = "R language script: @j363j  Data source: FiveThirtyEight.com") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) + 
   scale_colour_manual(name = " ",
-                      values=c("ElectoralBiden"="blue",
-                               "ElectoralTrump"="red"),
+                      values=c("ElectoralBiden"="blue", "ElectoralTrump"="red"),
                       labels=c("Biden", "Trump"))
 
 ggplot(data = election2) +
   geom_line(aes(x = Date, y = PopularBiden, group=1,
                 color = "PopularBiden"), size = 2) +
-  geom_text(aes(x = max(Date), y = max(PopularBiden),
-                label = round(max(PopularBiden), 0))) +
+  geom_text(aes(x = max(Date), y = PopularBiden[Date == max(Date)],
+                label = round(PopularBiden[Date == max(Date)], 0))) +
   geom_line(aes(x = Date, y = PopularTrump, group=1,
                 color = "PopularTrump"), size = 2) +
-  geom_text(aes(x = max(Date), y = max(PopularTrump),
-                label = round(max(PopularTrump), 0))) +
+  geom_text(aes(x = max(Date), y = PopularTrump[Date == max(Date)],
+                label = round(PopularTrump[Date == max(Date)], 0))) +
   labs(x="Date", y="Popular Votes",
        title="Election 2020 - Popular Vote",
        caption = "R language script: @j363j  Data source: FiveThirtyEight.com") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) + 
   scale_colour_manual(name = " ",
-                      values=c("PopularBiden"="blue",
-                               "PopularTrump"="red"),
+                      values=c("PopularBiden"="blue", "PopularTrump"="red"),
                       labels=c("Biden", "Trump"))
